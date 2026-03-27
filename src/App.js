b44e0b29d21bbc83e0a1e2c60922f1565a19103d
ngohoainamss1 <ngohoainamss1@gmail.com>
2026-03-28 03:15:40 +0700

    init-project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+
+import MyComponent from './components/MyComponent';
+
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          xinb cbia.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+
+    <div>
+      Helloao oaidoa
+      <MyComponent></MyComponent>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       xinb cbia.
+    //       <MyComponent></MyComponent>
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/MyComponent.js
@@ -0,0 +1,19 @@
+//Class component
+// function component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    // JSX
+    render() {
+        return (
+
+            <div>My fist Component
+                {Math.random()}
+
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
