8e7d975cf2078dc03ab136d8871cc6c8167c0eaa
ngohoainamss1 <ngohoainamss1@gmail.com>
2026-03-28 17:11:09 +0700

    init-project

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,11 +3,33 @@ import UserInfor from "./Userinfor";
 
 class DisplayInfor extends React.Component {
 
+
     render() {
+
+        const { listuser } = this.props;
+        console.log(listuser)
         return (
             <div>
-                <div> My name Nam</div>
-                <div> Age 26</div>
+                {listuser.map((item) => {
+                    console.log(item)
+                    return (
+                        <div key={item.id}>
+                            <div> My name {item.name}</div>
+                            <div> Age {item.age}</div>
+                            <hr />
+                        </div>
+                    )
+                })}
+                {/* <div> My name {name}</div>
+                <div> Age {age} </div>
+                <hr />
+                <div> My name {name}</div>
+                <div> Age {age} </div>
+                <hr />
+                <div> My name {name}</div>
+                <div> Age {age} </div> */}
+                {/* <div> My name {this.props.name}</div>
+                <div> Age {this.props.age} </div> */}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,13 +7,25 @@ import DisplayInfor from "./Display";
 
 class MyComponent extends React.Component {
 
+    state = {
+        listuser: [
+            { id: 1, name: "nammm", age: "25" },
+            { id: 2, name: "adadad", age: "33" },
+            { id: 3, name: "nammafafafm", age: "44" }
+        ]
+    }
+
     // JSX
     render() {
         return (
-
             <div>
                 <UserInfor></UserInfor>
-                <DisplayInfor></DisplayInfor>
+                <DisplayInfor
+                    listuser={this.state.listuser}
+                    user={this.state.listuser}
+
+                >
+                </DisplayInfor>
             </div>
         );
     }
