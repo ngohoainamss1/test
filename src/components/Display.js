8a1e2708facb812f2362d0a9ba2ecb9ea8f29f32
ngohoainamss1 <ngohoainamss1@gmail.com>
2026-03-28 15:59:59 +0700

    init-project

---
+++ src/components/Display.js
@@ -0,0 +1,17 @@
+import React from "react";
+import UserInfor from "./Userinfor";
+
+class DisplayInfor extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div> My name Nam</div>
+                <div> Age 26</div>
+            </div>
+        )
+    }
+
+}
+
+export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,54 +2,18 @@
 // function component
 
 import React from "react";
+import UserInfor from "./Userinfor";
+import DisplayInfor from "./Display";
 
 class MyComponent extends React.Component {
-
-    state = {
-        name: 'Eric',
-        address: 'Toi la ai',
-        age: 26
-    };
-
-    handleClick = (event) => {
-        console.log(">> cliack me buuton")
-        console.log("My name is", this.state.name)
-
-        this.setState({
-            name: "Nam"
-        })
-    }
-
-    handleMouseOver() {
-        console.log("hi hi hi")
-    }
-
-    thaydoiinput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        alert("name")
-    }
-
 
     // JSX
     render() {
         return (
 
             <div>
-                My name is {this.state.name} and i'am from {this.state.address}
-                <button onClick={this.handleClick}>Click me</button>
-                <button onMouseOver={this.handleMouseOver}>Click me</button>
-
-                <form onSubmit={(event) => this.handleSubmit(event)}>
-                    <input type="text"
-                        onChange={(event) => this.thaydoiinput(event)}></input>
-                    <button> SUbmit</button>
-                </form>
+                <UserInfor></UserInfor>
+                <DisplayInfor></DisplayInfor>
             </div>
         );
     }
