6cdbee9b7decb269b0337743f67f61cd1923b450
ngohoainamss1 <ngohoainamss1@gmail.com>
2026-03-28 18:25:30 +0700

    init-project

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,14 @@ import React from "react";
 import UserInfor from "./Userinfor";
 
 class DisplayInfor extends React.Component {
-
+    state = {
+        isShow: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShow: !this.state.isShow
+        })
+    }
 
     render() {
 
@@ -10,17 +17,49 @@ class DisplayInfor extends React.Component {
         console.log(listuser)
         return (
             <div>
-                {listuser.map((item) => {
-                    console.log(item)
-                    return (
-                        <div key={item.id}>
-                            <div> My name {item.name}</div>
-                            <div> Age {item.age}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
-                {/* <div> My name {name}</div>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShow === true ? "Hide list user" : "Show list user"}
+                    </span>
+                </div>
+                {this.state.isShow &&
+                    <div>
+                        {listuser.map((item) => {
+                            console.log(item)
+                            return (
+                                <div key={item.id} className={+item.age > 30 ? "green" : "red"}>
+                                    <div> My name {item.name}</div>
+                                    <div> Age {item.age}</div>
+                                    <hr />
+                                </div>
+                            )
+
+
+
+
+
+                            // if (+item.age > 30) {
+                            //     return (
+                            //         <div key={item.id} className="red">
+                            //             <div> My name {item.name}</div>
+                            //             <div> Age {item.age}</div>
+                            //             <hr />
+                            //         </div>
+                            //     )
+                            // }
+                            // else {
+                            //     return (
+                            //         <div key={item.id} className="green">
+                            //             <div> My name {item.name}</div>
+                            //             <div> Age {item.age}</div>
+                            //             <hr />
+                            //         </div>
+                            //     )
+                            // }
+                        })}
+
+
+                        {/* <div> My name {name}</div>
                 <div> Age {age} </div>
                 <hr />
                 <div> My name {name}</div>
@@ -28,8 +67,10 @@ class DisplayInfor extends React.Component {
                 <hr />
                 <div> My name {name}</div>
                 <div> Age {age} </div> */}
-                {/* <div> My name {this.props.name}</div>
+                        {/* <div> My name {this.props.name}</div>
                 <div> Age {this.props.age} </div> */}
+                    </div>
+                }
             </div>
         )
     }
